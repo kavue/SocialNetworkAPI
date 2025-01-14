e20191679f3702dd7a50c576a2cf4ac718b8c83d
kavue <kvue06@gmail.com>
2025-01-13 18:40:27 -0600

    Added directories & files

---
+++ src/models/User.ts
@@ -0,0 +1,41 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /.+\@.+\..+/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+    id: false,
+  }
+);
+
+// Virtual for friend count
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+export const User = model('User', userSchema);
